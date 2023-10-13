40857184c94e4eb2c3df07baa14ea89bd5895d23
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-10-13 16:32:40 +0200

    Add build-all script and fix some build issues.
    - support fancy patch levels in support script
    - relax .nvmrc
    - install build package in build-all script

--- build-scripts/env.cjs
+++ build-scripts/env.cjs
@@ -22,7 +22,7 @@ module.exports = {
   version() {
     const version = fs
       .readFileSync(path.resolve(paths.root_dir, "pyproject.toml"), "utf8")
-      .match(/version\W+=\W"(\d{8}\.\d(?:\.dev)?)"/);
+      .match(/version\W+=\W"(\d{8}\.\d+(?:\.dev)?)"/);
     if (!version) {
       throw Error("Version not found");
     }
